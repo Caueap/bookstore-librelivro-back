d231b3a671fbb3db8ea06c9a2669ce2543d24a08
Cauê <caueabproenca@gmail.com>
2022-08-03 16:19:17 -0300

    Created entity publisher

---
+++ src/main/java/com/api2/bookstore/models/PublisherModel.java
@@ -0,0 +1,45 @@
+package com.api2.bookstore.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PUBLISHER")
+public class PublisherModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String city;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
