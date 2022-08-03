48ad3cfc751f8c1e115c5f6d58d2cf8ac61f5973
Cauê <caueabproenca@gmail.com>
2022-08-03 16:41:13 -0300

    Created publisher repository

---
+++ src/main/java/com/api2/bookstore/repositories/PublisherRepository.java
@@ -0,0 +1,12 @@
+package com.api2.bookstore.repositories;
+
+import com.api2.bookstore.models.PublisherModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<PublisherModel, UUID> {
+
+}
