d71972e43b782a3c042564b1b9e751e161c97a09
Cauê <caueabproenca@gmail.com>
2022-08-09 15:17:24 -0300

    Created method get for client

--- src/main/java/com/api2/bookstore/services/ClientService.java
+++ src/main/java/com/api2/bookstore/services/ClientService.java
@@ -5,6 +5,7 @@ import com.api2.bookstore.repositories.ClientRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -18,5 +19,9 @@ public class ClientService {
     @Transactional
     public ClientModel save(ClientModel clientModel) {
         return clientRepository.save(clientModel);
+    }
+
+    public List<ClientModel> findAll() {
+        return clientRepository.findAll();
     }
 }
